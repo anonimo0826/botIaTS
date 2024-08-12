273a658f3642886aba3df7d82c54ee6781010301
Oliver Mendez <mendezoliver2108@gmail.com>
2024-08-12 17:11:46 -0500

    token

--- src/app.ts
+++ src/app.ts
@@ -61,8 +61,8 @@ const fullSamplesFlow = addKeyword<Provider, Database>(['samples', utils.setEven
 const main = async () => {
     const adapterFlow = createFlow([welcomeFlow, registerFlow, fullSamplesFlow])
     const adapterProvider = createProvider(Provider, {
-        jwtToken: process.env.JWT_TOKEN,
-        numberId: process.env.NUMBER_ID,
+        jwtToken: 'EAAYgzcqAN3oBO10AewRZCsOVWTLrlG9xK7yHlEcOfaPd1gumwVFH9HiNCQZCzGoT86MjTIaJxqWHGd1gY5tZBg58iu1c04FDbo5l4pMdUvIT7TQZCOrEgvL3OQu7IfxRJzfkeZBrd14hT2tc83RYa8nI1ajNQ6gUxsHkHY49LYL7p2Akxw6kTNyMdY40BrI35R3RnZAlTKN1c76vM5cj2z3BjQNSzNtGZA1As4ZD',
+        numberId: '392059530655358',
         verifyToken: 'Safiro2024',
         version: 'v18.0'
     })
