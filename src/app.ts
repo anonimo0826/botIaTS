3a3dad985fc04d603eeea0e9f8572ee76aad66de
Oliver Mendez <mendezoliver2108@gmail.com>
2024-08-12 17:23:49 -0500

    flow hi

--- src/app.ts
+++ src/app.ts
@@ -4,9 +4,12 @@ import { MetaProvider as Provider } from '@builderbot/provider-meta'
 
 const PORT = process.env.PORT ?? 3008
 
+const hello = addKeyword(['hola'])
+.addAnswer("Hola, Cual es tu nombre")
+
 const main = async () => {
 
-    const adapterFlow = createFlow([])
+    const adapterFlow = createFlow([hello])
     const adapterProvider = createProvider(Provider, {
         jwtToken: 'EAAYgzcqAN3oBO10AewRZCsOVWTLrlG9xK7yHlEcOfaPd1gumwVFH9HiNCQZCzGoT86MjTIaJxqWHGd1gY5tZBg58iu1c04FDbo5l4pMdUvIT7TQZCOrEgvL3OQu7IfxRJzfkeZBrd14hT2tc83RYa8nI1ajNQ6gUxsHkHY49LYL7p2Akxw6kTNyMdY40BrI35R3RnZAlTKN1c76vM5cj2z3BjQNSzNtGZA1As4ZD',
         numberId: '392059530655358',
