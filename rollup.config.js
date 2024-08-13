51ca46ae5ddc1f839f3c70067e318d4aabeb0dc9
Oliver Mendez <mendezoliver2108@gmail.com>
2024-08-12 20:10:06 -0500

    rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,5 @@
-import typescript from 'rollup-plugin-typescript2'
+import typescript from 'rollup-plugin-typescript2';
+import replace from '@rollup/plugin-replace';
 
 export default {
     input: 'src/app.ts',
@@ -7,7 +8,30 @@ export default {
         format: 'esm',
     },
     onwarn: (warning) => {
-        if (warning.code === 'UNRESOLVED_IMPORT') return
+        if (warning.code === 'UNRESOLVED_IMPORT') return;
     },
-    plugins: [typescript()],
-}
+    plugins: [
+        replace({
+            'process.env.OPENAI_API_KEY': JSON.stringify(process.env.OPENAI_API_KEY),
+            'process.env.JWT_TOKEN': JSON.stringify(process.env.JWT_TOKEN),
+            'process.env.NUMBER_ID': JSON.stringify(process.env.NUMBER_ID),
+            preventAssignment: true,
+        }),
+        typescript(),
+    ],
+};
+
+
+// import typescript from 'rollup-plugin-typescript2'
+
+// export default {
+//     input: 'src/app.ts',
+//     output: {
+//         file: 'dist/app.js',
+//         format: 'esm',
+//     },
+//     onwarn: (warning) => {
+//         if (warning.code === 'UNRESOLVED_IMPORT') return
+//     },
+//     plugins: [typescript()],
+// }
