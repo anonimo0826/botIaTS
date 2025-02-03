fbd8dbb7b052e7c8fff0782b313408a313a8c551
Oliver Mendez <mendezoliver2108@gmail.com>
2025-02-03 14:03:05 -0500

    new token

--- src/provider/index.ts
+++ src/provider/index.ts
@@ -1,7 +1,7 @@
 import { createProvider} from '@builderbot/bot'
 import { MetaProvider as Provider } from '@builderbot/provider-meta'
 export default createProvider(Provider, {
-    jwtToken: 'EAAYgzcqAN3oBO0H8P7ts75RPxUctzoejGeYKbEIuVwFlSXsAFEfMpUOhgdvzY95lZAJaILZBJMvbD4DoM57Cv2I9fZBAZAsPHTEoyDJzAn0d2sdQnUdsGkcmYrmLn81XSaYQ9y2k5yPLNKPeEvcbPeqCSCBH9MEdWXYunjMdwHUzDN8MhQbZBwDy3ZApQxbNw1CuZC3QmqKQH8AxRyJD9ZCtsnIzOBDtHT2cDlZCTkWZAC6mkZD',
+    jwtToken: 'EAAYgzcqAN3oBOZCzDm5nSoiVjk3j9Q9dBtZAH78HZCU5sWmIVwQPTVPegHi8pZCy0vYRZBg2uIZAA9V2XfPbqWZCmlIk02b2TU41nUOHNczbcGvPJ1HmrOVWG2QuzCrzre6LUl5okGHisL6Vjg81TPeFUZBaPvMdLjjyVlBM4fqQZCiH0i8RvuOXlwYznFRkZBcgKbHUO7ZC9iv41Ki4zsMVvQvhLMBi2Bfxh84g6p9XLqN1ZAUZD',
     numberId: '392059530655358',
     verifyToken: 'Safiro2024',
     version: 'v18.0'
