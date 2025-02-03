f75bf1e8d05ab1280a6054c8b5a860f17b451e67
Oliver Mendez <mendezoliver2108@gmail.com>
2025-02-03 15:02:51 -0500

    probando

--- src/provider/index.ts
+++ src/provider/index.ts
@@ -4,5 +4,6 @@ export default createProvider(Provider, {
     jwtToken: 'EAAYgzcqAN3oBOZCzDm5nSoiVjk3j9Q9dBtZAH78HZCU5sWmIVwQPTVPegHi8pZCy0vYRZBg2uIZAA9V2XfPbqWZCmlIk02b2TU41nUOHNczbcGvPJ1HmrOVWG2QuzCrzre6LUl5okGHisL6Vjg81TPeFUZBaPvMdLjjyVlBM4fqQZCiH0i8RvuOXlwYznFRkZBcgKbHUO7ZC9iv41Ki4zsMVvQvhLMBi2Bfxh84g6p9XLqN1ZAUZD',
     numberId: '392059530655358',
     verifyToken: 'Safiro2024',
+    
     version: 'v18.0'
 })
